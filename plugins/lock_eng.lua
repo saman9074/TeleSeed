cbacc70de16d3dcc222aa432e2c6ac0aa433041f
saman9074 <saman9074@yahoo.com>
2017-03-13 17:49:51 +0330

    Update lock_eng.lua

--- plugins/lock_eng.lua
+++ plugins/lock_eng.lua
@@ -1,33 +1,31 @@
-antienglish = {}-- An empty table for solving multiple kicking problem
+antieng = {}-- An empty table for solving multiple kicking problem
 
 do
 local function run(msg, matches)
-if msg.to.type == 'chat' then
   if is_momod(msg) then -- Ignore mods,owner,admins
     return
   end
   local data = load_data(_config.moderation.data)
   if data[tostring(msg.to.id)]['settings']['lock_eng'] then
     if data[tostring(msg.to.id)]['settings']['lock_eng'] == 'yes' then
-      if antienglish[msg.from.id] == true then 
+      if antieng[msg.from.id] == true then 
         return
       end
-      send_large_msg("chat#id".. msg.to.id , "⛔️ حروف انگلیسی مجاز نمیباشد.")
+      send_large_msg("chat#id".. msg.to.id , "English is not allowed here")
       local name = user_print_name(msg.from)
-      savelog(msg.to.id, name.." ["..msg.from.id.."] kicked (english was locked) ")
+      savelog(msg.to.id, name.." ["..msg.from.id.."] kicked (English was locked) ")
       chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id,ok_cb,false)
-		  antienglish[msg.from.id] = true
+		  antieng[msg.from.id] = true
       return
     end
   end
   return
 end
-end
 local function cron()
-  antienglish = {} -- Clear antienglish table 
+  antieng = {} -- Clear antiarabic table 
 end
 return {
-  patterns = {
+    patterns = {
     "(a)",
 	"(o)",
 	"(l)",
@@ -41,7 +39,23 @@ return {
 	"(I)",
 	"(u)",
 	"(U)",
-	"(m)"
+	"(m)",
+	"(B)",
+	"(b)",
+	"(T)",
+ 	"(t)",
+	"(J)",
+	"(j)",	
+	"(K)",
+	"(F)",
+	"(f)",
+	"(N)",
+	"(n)",
+	"(z)",
+	"(Z)",
+	"(X)",		
+	"(x)",
+	"(Y)"		
     },
   run = run,
 	cron = cron
